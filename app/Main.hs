fc4d965460f94717f614b76fadc5027d6c71e227
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2022-07-21 11:27:21 +0100

    Replace alacritty terminal with kitty

--- app/Main.hs
+++ app/Main.hs
@@ -41,7 +41,7 @@ main = do
 
     myModMask    = mod4Mask -- Super (Windows) key
     myXMonadDir  = "~/Projects/xmonad-quark/"
-    myTerminal   = "alacritty"
+    myTerminal   = "kitty"
     myBar        = XMobar
     myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
